094814219e9294348432154bbc11b8b12f307241
Ezra Garber <ezra@gahooa.com>
2013-01-11 19:15:24 -0800

    Times 2

--- Web/times2.py
+++ Web/times2.py
@@ -8,7 +8,7 @@ htmlstart = '''
 <html>
   <head>
     <style>
-      html, body, div, form, fieldset, legend, label {margin: 0; padding: 0; }
+      html, body, div, form, fieldset, legend, label {margin: 0; padding: 0; font-family: verdana;}
       table {border-collapse: collapse; border-spacing: 0; }
       th, td {text-align: left; vertical-align: top; }
       h1, h2, h3, h4, h5, h6, th, td, caption { font-weight:normal; }
@@ -26,19 +26,51 @@ htmlstart = '''
         border: 2px solid #666;
         border-collapse: collapse;
         text-align: center;
-        vertical-algin: middle;
+        vertical-align: middle;
+        width: 60px;
+        height: 60px;
+        font-family: 'courier new';
+        font-size: 12pt;
       }
+
+      div.Box
+      {
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        width: 100px;
+        height: 100px;
+        background-color: blue;
+        border: 1px solid black;
+      }
+
     </style>
   </head>
   <body>
 '''
 
 htmlend = '''
+    <div class="Box"></div>
+    <div class="Box"></div>
+    <div class="Box"></div>
+    <div class="Box"></div>
+    <div class="Box"></div>
+    <div class="Box"></div>
+    <div class="Box"></div>
+    <div class="Box"></div>
+    <div class="Box"></div>
+    <div class="Box"></div>
+    
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script> 
+    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"></script> 
+    
+    <script>
+      $(function(){
+        $('div.Box').draggable();
+      });
+
+    </script>
   </body>
-  <script>
-
-
-  </script>
 </html>
 '''
 
@@ -46,25 +78,62 @@ htmlend = '''
 @Expose
 def Request(self):
   yield
-  
+ 
+  w1 = IN_Int(self.Get.w1) or 1
+  w2 = IN_Int(self.Get.w2) or 12
+  h1 = IN_Int(self.Get.h1) or 1
+  h2 = IN_Int(self.Get.h2) or 12
+
   W = self.HTMLResponse()
   W(htmlstart)
       
-  W('<table>')
-
-  for y in range(1,1001):
-    W('<tr>')
-    
-    for x in range(1,13):
-      W('<td>')
-      W(str(x*y))
-      W('</td>')
-    
-    W('</tr>')
+   
   
+  W('''
+    <form name="F" method="get">
+      Width (start, end): 
+      <input type="text" name="w1" value=''' + QA(w1) + ''' />
+      <input type="text" name="w2" value=''' + QA(w2) + ''' />
+      <br />
+      Height (start, end): 
+      <input type="text" name="h1" value=''' + QA(h1) + ''' />
+      <input type="text" name="h2" value=''' + QA(h2) + ''' />
+      <br />
+      <input type="submit" value="Update!" />
+      or <a href="?">Reset</a>
+    </form>
 
-  W('</table>')
+    <br />
+    <br />
+  
+    <table>
+    ''')
 
+  W('<tr>\n')
+  W('<td><strong style="font-size: 200%;">X</strong></td>')
+    
+  for x in range(w1,w2+1):
+    W('<td><strong>' + str(x) + '</strong></td>\n')
+    
+  W('</tr>\n')
+  
+  i = 0
+  
+  for y in range(h1,h2+1):
+    W('<tr>\n')
+    
+    W('<td><strong>' + str(y) + '</strong></td>\n')
+    
+    for x in range(w1,w2+1):
+      W('<td>' + str(x*y) + '</td>\n')
+      i += 1
+      if i > 10000:
+        raise Exception('TOO BIG')
+          
+    W('</tr>\n')
+
+  W('</table>\n')
+  
   W(htmlend)
 
   yield W

244761aeeeff34a48c99c5a3a90f403a5333cdcc
Ezra Garber <ezra@gahooa.com>
2013-02-07 19:28:39 -0800

    Added gaug.es tracking for LIVE

--- Web/__init__.py
+++ Web/__init__.py
@@ -87,6 +87,20 @@ def Init(self):
   self.UI.Nav1.Add('/story/', Name='Stories') 
   self.UI.Nav1.Add('/trystuff/', Name='Try Stuff') 
 
+  if App.DevLevel == 0:
+    self.UI.Script('''
+      var _gauges = _gauges || [];
+      (function() {
+      var t   = document.createElement('script');
+      t.type  = 'text/javascript';
+      t.async = true;
+      t.id    = 'gauges-tracker';
+      t.setAttribute('data-site-id', '51147057108d7b16a4000049');
+      t.src = '//secure.gaug.es/track.js';
+      var s = document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(t, s);
+      })();
+      ''')
 
 
   yield
